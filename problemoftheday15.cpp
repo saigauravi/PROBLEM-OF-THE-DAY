8d499c39d025a8ec44c4b523afa60f9d2f31d000
saigauravi <96459092+saigauravi@users.noreply.github.com>
2022-01-15 01:36:22 +0530

    POTD15

---
+++ problemoftheday15.cpp
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+bool findPair(int arr[], int size, int n);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int l,n;
+        cin>>l>>n;
+        int arr[l];
+        for(int i=0;i<l;i++)
+            cin>>arr[i];
+        if(findPair(arr, l, n))
+            cout<<1<<endl;
+        else cout<<"-1"<<endl;
+    }
+
+
+    return 0;
+}// } Driver Code Ends
+
+
+bool findPair(int a[], int size, int n){
+   for(int i=0;i<size;i++)
+   {
+       for(int j=0;j<size;j++)
+       {
+           if(a[i]-a[j]==n)
+           return true;
+       }
+   }
+   return false;
+}
